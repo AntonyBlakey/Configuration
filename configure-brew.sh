68ac639cac75cded72b4816307deb827461b4c1e
Antony Blakey <antony.blakey@gmail.com>
2017-06-22 09:06:06 +0200

    Recover configure-brew.sh

---
+++ configure-brew.sh
@@ -0,0 +1,67 @@
+brew install git
+brew install mercurial
+brew install gnu-sed
+brew install gawk
+brew install vim
+brew install tmux
+brew install jump
+brew install desk
+brew install fzf
+brew install wget
+brew install emacs --with-cocoa --with-modules
+brew install translate-shell
+brew install node
+brew install haskell-stack
+brew install purescript
+npm install -g pulp
+brew install leiningen
+brew install elixir
+brew install ocaml
+brew install opam
+rm -rf ~/.opam
+opam init
+brew install nim
+brew install rust
+brew install packer
+brew install cmake
+brew install clozure-cl
+brew install sbcl
+brew install ecl
+brew install swi-prolog
+brew install mercury
+brew install elm
+brew install idris
+brew install msgpack
+brew tap neovim/neovim
+brew install neovim
+pip install --upgrade pip
+pip install sphinx
+brew install ninja
+brew install doxygen
+brew install roswell
+brew install mitmproxy
+brew install graphviz --with-librsvg
+brew install homebrew/versions/autoconf213
+brew install yasm
+brew install ag
+brew tap homebrew/emacs
+brew install tern --with-auto-complete
+brew install httrack
+brew install lumo
+brew install swig
+brew install rtags
+brew install clang-format
+brew install platypus
+brew install logtalk --with-swi-prolog
+brew install mongodb
+brew install libtensorflow
+brew install meson
+brew cask install racket
+brew install chicken
+brew install exa
+brew install jsonlint
+brew install thefuck
+brew install gbevin/tools/receivemidi
+brew install gbevin/tools/sendmidi
+brew install nnn
+brew install macvim
